b679f8f506c94f9a6bcdd20d4fb6dcf8307d3b25
adamelevate <adamelevate@gmail.com>
2015-09-23 17:54:22 -0500

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hintApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hintApp
+ */
+angular.module('hintApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
